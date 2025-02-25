0e1bd4505acde1213b3e08e7941bfd713d9f9fe0
evroman03 <evroman@mail.bradley.edu>
2025-02-25 10:29:28 -0600

    UpdateCSVReader

---
+++ MidtermMilestoneProject/MidtermMilestoneProject/CSVReader.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+class CSVReader {
+public:
+    static vector<vector<string>> readCSV(const string& filename) {
+        ifstream file(filename);
+        vector<vector<string>> data;
+        string line, word;
+
+        if (!file.is_open()) {
+            cerr << "Error: Could not open file " << filename << endl;
+            return data;
+        }
+
+        while (getline(file, line)) {
+            stringstream ss(line);
+            vector<string> row;
+            while (getline(ss, word, ',')) {
+                row.push_back(word);
+            }
+            data.push_back(row);
+        }
+        file.close();
+        return data;
+    }
+};
