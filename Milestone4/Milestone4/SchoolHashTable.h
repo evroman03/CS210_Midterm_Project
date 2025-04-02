285ad34555123dbb44c59a5f3ca206b99ff8d6fe
evroman03 <evroman@mail.bradley.edu>
2025-04-01 22:18:39 -0500

    Update SchoolHashTable.h
    Starting insert and delete, no rehashing

--- Milestone4/Milestone4/SchoolHashTable.h
+++ Milestone4/Milestone4/SchoolHashTable.h
@@ -30,7 +30,29 @@ private:
 public:
     SchoolHashTable(int size = 10) : tableSize(size), table(size) {}
 
-   
+    void insert(const SchoolData& school) 
+    {
+        int index = hashFunction(school.name);
+        table[index].push_back(school);
+    }
+    bool deleteByName(const string& name)
+    {
+        int index = hashFunction(name);
+        auto& schoolNodeChain = table[index];
+
+        //go through the WHOLE list at the index recieved from the hashing 
+        for (auto it = schoolNodeChain.begin(); it != schoolNodeChain.end(); ++it)
+        {
+            if (it->name == name)
+            {
+                //Erase looks to be used for vectors queues and lists. Just removing
+                //at the index 'it'
+                schoolNodeChain.erase(it);
+                return true;
+            }
+        }
+        return false;
+    }
 };
 
 #endif
