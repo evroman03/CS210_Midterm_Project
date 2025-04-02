950e6751f970441f35849f8c3eb044efbe42e971
evroman03 <evroman@mail.bradley.edu>
2025-04-01 22:11:41 -0500

    Create Schoolhashtable
    Just got all the starter stuff out of the way

--- Milestone4/Milestone4/SchoolHashTable.h
+++ Milestone4/Milestone4/SchoolHashTable.h
@@ -1,1 +1,36 @@
-#pragma once
+#ifndef SCHOOLHASHTABLE_H
+#define SCHOOLHASHTABLE_H
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include "SchoolData.h"
+using namespace std;
+
+//using chaining/chained hashing bc it seems easier to just use lists than create
+//a proper addressing :P 
+//Plus it's not that bad
+class SchoolHashTable
+{
+private:
+    int tableSize;
+    vector<list<SchoolData>> table;
+
+    int hashFunction(const string& name) 
+    {
+        int hash = 0;
+        for (char ch : name) {
+            hash += ch;
+        }
+        return hash % tableSize;
+    }
+
+   
+
+public:
+    SchoolHashTable(int size = 10) : tableSize(size), table(size) {}
+
+   
+};
+
+#endif
